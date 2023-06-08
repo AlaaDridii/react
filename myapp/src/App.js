317c3d1f7e1ba98dbdd077c24435d055c644fe9d
AlaaDridii <127346208+AlaaDridii@users.noreply.github.com>
2023-06-08 20:53:40 +0100

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Slideim from './composant/Slideim';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Slideim from './composant/Slideim'; //importer le composant Slideim dans App.js
+import 'bootstrap/dist/css/bootstrap.min.css'; //importer le CSS from boostrap 
 
 
 function App() {
   return (
     <>
-    <Slideim/>
+    <Slideim/> 
     </>
   );
 }
